4384f4bc09eb80ff080ae5470feee0a086022787
cxvetown <56442908+cxvetown@users.noreply.github.com>
2021-05-09 14:02:37 -0400

    pelicula class

---
+++ Negocio/Pelicula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class Pelicula
+    {
+        int duracion_minutos;
+        int minuto;
+
+        public int Duracion_minutos { get => duracion_minutos; set => duracion_minutos = value; }
+        public int Minuto { get => minuto; set => minuto = value; }
+
+        public override string ToString()
+        {
+            return $"Minutos totales: { Duracion_minutos}, Minuto: {Minuto}";
+        }
+    }
+}
