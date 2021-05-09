df009696a0ae6ac04f9cc5fc767647100df5f44e
SetHet <36393893+SetHet@users.noreply.github.com>
2021-05-09 19:11:17 -0400

    Start BD module

---
+++ BD/DataBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace BD
+{
+    public class DataBase
+    {
+        public string nameDB = "myBackLogDataBase.sqlite3";
+        public string connectionString = "Data Source=database.sqlite3";
+
+        public SQLiteConnection myConnection;
+
+        public DataBase()
+        {
+            myConnection = new SQLiteConnection(connectionString);
+
+            if (!File.Exists("./" + nameDB))
+            {
+                try
+                {
+                    SQLiteConnection.CreateFile(nameDB);
+                    BD.SQLiteModel.CrearModelo(this);
+
+                    System.Console.WriteLine(">> Creada la base de datos: " + nameDB);
+                }
+                catch(Exception ex)
+                {
+                    System.Console.WriteLine(">> ERROR: No se ha podido crear la base de datos\n" + ex.Message);
+                }
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
+
+//Guide: https://www.youtube.com/watch?v=anTP-mgktiI&ab_channel=OverSeasMedia
---
+++ BD/SQLiteModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD
+{
+    public class SQLiteModel
+    {
+        public static void CrearModelo(BD.DataBase db)
+        {
+            //Crear tablas
+            CreateTables();
+
+            //Agregar Registros
+            StartLogs();
+        }
+
+        public static void CreateTables()
+        {
+
+        }
+
+        public static void StartLogs()
+        {
+
+        }
+    }
+}
