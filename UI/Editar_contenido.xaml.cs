5effca3e2c91b279f3dc6f2a210561c52d3e06bf
cxvetown <56442908+cxvetown@users.noreply.github.com>
2021-05-15 21:16:28 -0400

    intento modificar contenido

---
+++ UI/Editar_contenido.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Lógica de interacción para Editar_contenido.xaml
+    /// </summary>
+    public partial class Editar_contenido : Window
+    {
+        public Editar_contenido()
+        {
+            InitializeComponent();
+        }
+
+        private void salir_editar_Click(object sender, RoutedEventArgs e)
+        {
+            Modificar_contenido modificar = new Modificar_contenido();
+            modificar.Show();
+            this.Close();
+        }
+
+        private void editar_subtipo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UI/Modificar_contenido.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Lógica de interacción para Modificar_contenido.xaml
+    /// </summary>
+    public partial class Modificar_contenido : Window
+    {
+        public Modificar_contenido()
+        {
+            InitializeComponent();
+        }
+
+        private void addplataforma_Click(object sender, RoutedEventArgs e)
+        {
+            Editar_contenido editar = new Editar_contenido();
+            editar.Show();
+            this.Close();
+        }
+    }
+}
