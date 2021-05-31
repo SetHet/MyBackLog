bee5056add54b3b441fc295771d20d234429cdc8
SetHet <36393893+SetHet@users.noreply.github.com>
2021-05-30 21:35:46 -0400

    CRUD Juego y Nota

--- Datos/Nota.cs
+++ Datos/Nota.cs
@@ -9,16 +9,18 @@ namespace Datos
     public class Nota
     {
         int id_nota;
+        int id_contenido;
         string descripcion;
         bool completado;
 
         public int Id_nota { get => id_nota; set => id_nota = value; }
         public string Descripcion { get => descripcion; set => descripcion = value; }
         public bool Completado { get => completado; set => completado = value; }
+        public int Id_contenido { get => id_contenido; set => id_contenido = value; }
 
         public override string ToString()
         {
-            return $"id nota: {Id_nota}, descripcion: {Descripcion}, completado: {Completado}";
+            return $"id nota: {Id_nota}, id juego/contenido: {Id_contenido}, descripcion: {Descripcion}, completado: {Completado}";
         }
     }
 }
